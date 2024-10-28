a701bc3f38dd9b54af6793e0ae5c5ca03ad6545d
JTolentino1997 <jhonmartolentino021@gmail.com>
2024-10-28 16:35:32 +0800

    revise animation menu

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
 // JM ------------------
 
-
-let mybutton = document.getElementById("myBtn");
-     
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
-
+ 
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 window.onscroll = function() {scrollFunction()};
 
@@ -22,6 +13,27 @@ function scrollFunction() {
     document.getElementById("logo").style.fontSize = "30px";
   }
 }
+
+
+// Back to top button
+
+let mybutton = document.getElementById("myBtn");
+      
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+  // Show button when scrolling down 20px
+  window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  };
+
+
 
 // end jm---------------
 
